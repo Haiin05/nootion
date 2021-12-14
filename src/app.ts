e5face4dda51f0171ca82427f292bd3243f764f5
haiin <h986680@gmail.com>
2021-12-14 14:26:31 +0900

    add dependency injection - PageComponent

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,27 @@
+import {Component} from "./components/component.js";
 import {ImageComponent} from "./components/item/image.js";
 import {NoteComponent} from "./components/item/note.js";
 import {TodoComponent} from "./components/item/todo.js";
 import {VideoComponent} from "./components/item/video.js";
-import {PageComponent} from "./components/page/page.js";
+import {Composable, PageComponent, PageItemComponent} from "./components/page/page.js";
 
 class App {
-	private readonly page: PageComponent
+	private readonly page: Component & Composable
 	constructor (appRoot: HTMLElement) {
-		this.page = new PageComponent()
+		this.page = new PageComponent(PageItemComponent)
 		this.page.attachTo(appRoot)
 
 		const image = new ImageComponent('Image 1', 'https://picsum.photos/200/300')
-		image.attachTo(appRoot, 'beforeend')
+		this.page.addChild(image)
 
 		const note = new NoteComponent('Note 1', 'This is Note')
-		note.attachTo(appRoot, 'beforeend')
+		this.page.addChild(note)
 
 		const todo = new TodoComponent('Todo 1', 'Eat dinner')
-		todo.attachTo(appRoot, 'beforeend')
+		this.page.addChild(todo)
 
 		const video = new VideoComponent('Video 1', 'https://www.youtube.com/watch?v=m17WVyGy3dc')
-		video.attachTo(appRoot, 'beforeend')
+		this.page.addChild(video)
 	}
 }
 
